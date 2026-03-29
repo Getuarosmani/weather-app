c920210a6d71f413fc25d818fc48fd34d62f01fc
Getuar Osmani <getuarosmani91@gmail.com>
2026-03-29 13:49:01 +0200

    fix: removed default icon error

--- src/Componets/WeatherSection.jsx
+++ src/Componets/WeatherSection.jsx
@@ -3,8 +3,8 @@ export const WeatherSection = ({ currentWeather }) => {
   return (
     <div className=' text-center pb-10 border-b mb-3'>
       <img
-        src={`${import.meta.env.BASE_URL}icons/${currentWeather.weatherIcon || 'default-icon'}.svg`}
-        alt={currentWeather.description || 'Weather Icon'}
+        src={`${import.meta.env.BASE_URL}icons/${currentWeather.weatherIcon}.svg`}
+        alt="weather icon"
         className="mx-auto w-45 h-45" />
       <h2 className='text-6xl mb-5'>{Math.floor(currentWeather.temperature)}<span className='text-4xl'>°C</span></h2>
       <p className='text-lg'>{currentWeather.description}</p>
