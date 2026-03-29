2af99607b25deb1fad973435856fa01ea9abc76b
Getuar Osmani <getuarosmani91@gmail.com>
2026-03-29 04:02:09 +0200

    fix hourly icons github pages

--- src/Componets/HourlyForcest.jsx
+++ src/Componets/HourlyForcest.jsx
@@ -1,18 +1,26 @@
-import { weatherCodes } from "../constants"
+import { weatherCodes } from "../constants";
 
-
-export const HourlyForcest = ({hourlyWeather}) => {
+export const HourlyForcest = ({ hourlyWeather }) => {
 
     const temperature = Math.floor(hourlyWeather.temp_c);
-    const time = hourlyWeather.time.split(' ')[1].substring(0, 5);
-    const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(hourlyWeather.condition.code));
+    const time = hourlyWeather.time.split(" ")[1].substring(0, 5);
 
+    // 🔧 Fix: fallback icon nëse nuk gjendet match
+    const weatherIcon = Object.keys(weatherCodes).find(icon =>
+        weatherCodes[icon].includes(hourlyWeather.condition.code)
+    ) || "clear";
 
     return (
         <li className="mr-15 flex-shrink-0 text-center last:mr-0">
             <p>{time}</p>
-            <img src={`icons/${weatherIcon}.svg`} alt="cloud" className="w-10 h-10 mx-auto" />
+
+            <img
+                src={`${import.meta.env.BASE_URL}icons/${weatherIcon}.svg`}
+                alt="weather icon"
+                className="w-10 h-10 mx-auto"
+            />
+
             <p>{temperature}°</p>
         </li>
-    )
-}
+    );
+};
